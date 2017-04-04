cc4db47b92d7c6fc009decd2258d92f0771ce086
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-04-04 14:07:25 +0530

    Add sd_event loop and callback handler
    Change-Id: I032d1156be2b8082fdf347a60ec883a9bc0038ae
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- monitor.hpp
+++ monitor.hpp
@@ -2,7 +2,7 @@
 
 #include <unistd.h>
 #include <string>
-#include <iostream>
+#include <systemd/sd-event.h>
 namespace phosphor
 {
 namespace gpio
@@ -22,15 +22,24 @@ class Monitor
 
         /** @brief Constructs Monitor object.
          *
-         *  @param[in] path     - Path to gpio input device
-         *  @param[in] line     - GPIO state to monitor
+         *  @param[in] path    - Path to gpio input device
+         *  @param[in] state   - GPIO action that is of interest
+         *  @param[in] event   - sd_event handler
+         *  @param[in] handler - IO callback handler
          */
-        Monitor(const std::string& path, uint32_t state)
+        Monitor(const std::string& path, uint32_t state,
+                sd_event* event, sd_event_io_handler_t handler)
             : path(path),
-              state(state)
+              state(state),
+              event(event),
+              callbackHandler(handler)
+
         {
-            // Populate the file descriptor for the passed in device
+            // Populate the file descriptor for passed in device
             openDevice();
+
+            // And register callback handler when FD has some data
+            registerCallback();
         }
 
         ~Monitor()
@@ -39,20 +48,50 @@ class Monitor
             {
                 close(fd);
             }
+
+            if (eventSource)
+            {
+                eventSource = sd_event_source_unref(eventSource);
+            }
         }
+
+        /** @brief Callback handler when the FD has some activity on it
+         *
+         *  @param[in] es       - Populated event source
+         *  @param[in] fd       - Associated File descriptor
+         *  @param[in] revents  - Type of event
+         *  @param[in] userData - User data that was passed during registration
+         *
+         *  @return             - 0 or positive number on success and negative
+         *                        errno otherwise
+         */
+        static int processEvents(sd_event_source* es, int fd,
+                                 uint32_t revents, void* userData);
 
     private:
         /** @brief Absolute path of GPIO input device */
         const std::string& path;
 
-        /** @brief GPIO state that is of interest */
+        /** @brief GPIO state change of interest */
         uint32_t state;
 
         /** @brief File descriptor for the gpio input device */
         int fd = -1;
 
-        /** @brief Opens the device and populates the descriptor */
+        /** @brief Monitor to sd_event */
+        sd_event *event;
+
+        /** @brief event source */
+        sd_event_source* eventSource = nullptr;
+
+        /** @brief Callback handler when the FD has some data */
+        sd_event_io_handler_t callbackHandler;
+
+        /** @brief Populate the file descriptor for passed in device */
         void openDevice();
+
+        /** @brief attaches FD to events and sets up callback handler */
+        void registerCallback();
 };
 
 } // namespace gpio
