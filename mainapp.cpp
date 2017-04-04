89ab33422e00bfd219ba37a4cfec22fa209ccb61
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-04-04 14:07:25 +0530

    Add sd_event loop and callback handler
    Change-Id: I032d1156be2b8082fdf347a60ec883a9bc0038ae
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- mainapp.cpp
+++ mainapp.cpp
@@ -16,9 +16,12 @@
 
 #include <iostream>
 #include <string>
+#include <systemd/sd-event.h>
+#include <phosphor-logging/log.hpp>
 #include "argument.hpp"
 #include "monitor.hpp"
 
+using namespace phosphor::logging;
 static void exitWithError(const char* err, char** argv)
 {
     phosphor::gpio::ArgumentParser::usage(argv);
@@ -28,6 +31,8 @@ static void exitWithError(const char* err, char** argv)
 
 int main(int argc, char** argv)
 {
+    sd_event* event = nullptr;
+
     // Read arguments.
     auto options = phosphor::gpio::ArgumentParser(argc, argv);
 
@@ -49,15 +54,27 @@ int main(int argc, char** argv)
         exitWithError("state not specified.", argv);
     }
 
-    // TODO : Convert the state to integer
-
     // Parse out target argument. It is fine if the caller does not
     // pass this if they are not interested in calling into any target
     // on meeting a condition.
     auto target = (options)["target"];
 
-    // Create a GPIO monitor object and let it do all the rest
-    phosphor::gpio::Monitor monitor(path, std::stoi(state), target);
+    auto r = sd_event_default(&event);
+    if (r < 0)
+    {
+        log<level::ERR>("Error creating a default sd_event handler");
+        return r;
+    }
+    phosphor::gpio::eventPtr eventP;
+    eventP.reset(event);
+    event = nullptr;
+
+    // Create a monitor object and let it do all the rest
+    phosphor::gpio::Monitor monitor(path, std::stoi(state),
+                                    target, eventP,
+                                    phosphor::gpio::Monitor::processEvents);
+    // Wait for events
+    sd_event_loop(eventP.get());
 
     return 0;
 }
--- monitor.hpp
+++ monitor.hpp
@@ -2,11 +2,23 @@
 
 #include <unistd.h>
 #include <string>
-#include <iostream>
+#include <systemd/sd-event.h>
 namespace phosphor
 {
 namespace gpio
 {
+
+/* Need a custom deleter for freeing up sd_event */
+struct EventDeleter
+{
+    void operator()(sd_event* event) const
+    {
+        event = sd_event_unref(event);
+    }
+};
+
+using eventPtr = std::unique_ptr<sd_event, EventDeleter>;
+
 /** @class Monitor
  *  @brief Responsible for catching GPIO state change
  *  condition and taking actions
@@ -22,19 +34,28 @@ class Monitor
 
         /** @brief Constructs Monitor object.
          *
-         *  @param[in] path     - Path to gpio input device
-         *  @param[in] line     - GPIO state to monitor
-         *  @param[in] target   - systemd unit to be started on GPIO
-         *                        state change
+         *  @param[in] path    - Path to gpio input device
+         *  @param[in] state   - GPIO action that is of interest
+         *  @param[in] target  - systemd unit to be started on GPIO
+         *                       state change
+         *  @param[in] event   - sd_event handler
+         *  @param[in] handler - IO callback handler
          */
         Monitor(const std::string& path, uint32_t state,
-                const std::string& target)
+                const std::string& target,
+                const eventPtr& event,
+                sd_event_io_handler_t handler)
             : path(path),
               state(state),
-              target(target)
+              target(target),
+              event(event),
+              callbackHandler(handler)
         {
-            // Populate the file descriptor for the passed in device
+            // Populate the file descriptor for passed in device
             openDevice();
+
+            // And register callback handler when FD has some data
+            registerCallback();
         }
 
         ~Monitor()
@@ -43,13 +64,31 @@ class Monitor
             {
                 close(fd);
             }
+
+            if (eventSource)
+            {
+                eventSource = sd_event_source_unref(eventSource);
+            }
         }
+
+        /** @brief Callback handler when the FD has some activity on it
+         *
+         *  @param[in] es       - Populated event source
+         *  @param[in] fd       - Associated File descriptor
+         *  @param[in] revents  - Type of event
+         *  @param[in] userData - User data that was passed during registration
+         *
+         *  @return             - 0 or positive number on success and negative
+         *                        errno otherwise
+         */
+        static int processEvents(sd_event_source* es, int fd,
+                                 uint32_t revents, void* userData);
 
     private:
         /** @brief Absolute path of GPIO input device */
         const std::string& path;
 
-        /** @brief GPIO state that is of interest */
+        /** @brief GPIO state change of interest */
         uint32_t state;
 
         /** @brief Systemd unit to be started when the condition is met */
@@ -58,8 +97,20 @@ class Monitor
         /** @brief File descriptor for the gpio input device */
         int fd = -1;
 
-        /** @brief Opens the device and populates the descriptor */
+        /** @brief Monitor to sd_event */
+        const eventPtr& event;
+
+        /** @brief event source */
+        sd_event_source* eventSource = nullptr;
+
+        /** @brief Callback handler when the FD has some data */
+        sd_event_io_handler_t callbackHandler;
+
+        /** @brief Populate the file descriptor for passed in device */
         void openDevice();
+
+        /** @brief attaches FD to events and sets up callback handler */
+        void registerCallback();
 };
 
 } // namespace gpio
