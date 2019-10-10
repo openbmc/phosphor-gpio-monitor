0ff3a276d631945223f81b240f08515c8b9bbe31
Vijay Khemka <vijaykhemka@fb.com>
2019-10-09 17:45:45 -0700

    New gpio monitor
    Added a new gpioMonitor which monitor multiple gpio lines based on their
    line name or offset. gpio details needs to be defined in a
    gpioMonConfig.json file and needs to be passed to this daemon.
    
    This uses libgpiod.
    
    Signed-off-by: Vijay Khemka <vijaykhemka@fb.com>
    Change-Id: I843e6df8c1159888f2ca628d1f69c1d2294d29d6

---
+++ gpioMon.cpp
@@ -0,0 +1,145 @@
+/**
+ * Copyright © 2019 Facebook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gpioMon.hpp"
+
+#include <phosphor-logging/log.hpp>
+#include <sdbusplus/bus.hpp>
+
+namespace phosphor
+{
+namespace gpio
+{
+
+/* systemd service to kick start a target. */
+constexpr auto SYSTEMD_SERVICE = "org.freedesktop.systemd1";
+constexpr auto SYSTEMD_ROOT = "/org/freedesktop/systemd1";
+constexpr auto SYSTEMD_INTERFACE = "org.freedesktop.systemd1.Manager";
+
+using namespace phosphor::logging;
+
+void GpioMonitor::gpioEventHandler()
+{
+    gpiod_line_event gpioLineEvent;
+    auto findLineName = gpiod_line_name(gpioLine);
+    std::string gpioLineMsg = "GPIO Line: ";
+
+    if (findLineName == NULL)
+    {
+        gpioLineMsg += std::to_string(gpiod_line_offset(gpioLine));
+    }
+    else
+    {
+        gpioLineMsg += findLineName;
+    }
+
+    if (gpiod_line_event_read_fd(gpioEventDescriptor.native_handle(),
+                                 &gpioLineEvent) < 0)
+    {
+        log<level::ERR>("Failed to read gpioLineEvent from fd");
+        return;
+    }
+
+    std::string logMessage =
+        "GPIO line " + gpioLineMsg +
+        (gpioLineEvent.event_type == GPIOD_LINE_EVENT_RISING_EDGE
+             ? " Asserted"
+             : " Deasserted");
+
+    log<level::INFO>(logMessage.c_str());
+
+    /* Execute the target if it is defined. */
+    if (!target.empty())
+    {
+        auto bus = sdbusplus::bus::new_default();
+        auto method = bus.new_method_call(SYSTEMD_SERVICE, SYSTEMD_ROOT,
+                                          SYSTEMD_INTERFACE, "StartUnit");
+        method.append(target);
+        method.append("replace");
+
+        bus.call_noreply(method);
+    }
+
+    /* if cnot required to continue monitoring then return */
+    if (!continueAfterKeyPress)
+    {
+        return;
+    }
+
+    gpioEventDescriptor.async_wait(
+        boost::asio::posix::stream_descriptor::wait_read,
+        [this](const boost::system::error_code& ec) {
+            if (ec)
+            {
+                log<level::ERR>("GPIO event handler error",
+                                entry("ERR_MSG=%s", ec.message().c_str()));
+                return;
+            }
+            gpioEventHandler();
+        });
+}
+
+int GpioMonitor::requestGPIOEvents()
+{
+
+    std::string errMsg;
+    auto findLineName = gpiod_line_name(gpioLine);
+    std::string gpioLineMsg = "GPIO Line: ";
+
+    if (findLineName == NULL)
+    {
+        gpioLineMsg += std::to_string(gpiod_line_offset(gpioLine));
+    }
+    else
+    {
+        gpioLineMsg += findLineName;
+    }
+
+    /* Request an event to monitor for respected gpio line */
+    if (gpiod_line_request(gpioLine, &gpioConfig, 0) < 0)
+    {
+        errMsg = "Failed to request events for " + gpioLineMsg;
+        log<level::ERR>(errMsg.c_str());
+        return -1;
+    }
+
+    int gpioLineFd = gpiod_line_event_get_fd(gpioLine);
+    if (gpioLineFd < 0)
+    {
+        errMsg = "Failed to get fd for " + gpioLineMsg;
+        log<level::ERR>(errMsg.c_str());
+        return -1;
+    }
+
+    gpioEventDescriptor.assign(gpioLineFd);
+
+    errMsg = gpioLineMsg + " fd handler error: ";
+
+    gpioEventDescriptor.async_wait(
+        boost::asio::posix::stream_descriptor::wait_read,
+        [errMsg, this](const boost::system::error_code& ec) {
+            if (ec)
+            {
+                std::string msg = errMsg + std::string(ec.message());
+                log<level::ERR>(msg.c_str());
+                return;
+            }
+            gpioEventHandler();
+        });
+    return 0;
+}
+} // namespace gpio
+} // namespace phosphor
---
+++ gpioMon.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <gpiod.h>
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/posix/stream_descriptor.hpp>
+
+namespace phosphor
+{
+namespace gpio
+{
+
+/** @class GpioMonitor
+ *  @brief Responsible for catching GPIO state change
+ *  condition and starting systemd targets.
+ */
+class GpioMonitor
+{
+  public:
+    GpioMonitor() = delete;
+    ~GpioMonitor() = default;
+    GpioMonitor(const GpioMonitor&) = delete;
+    GpioMonitor& operator=(const GpioMonitor&) = delete;
+    GpioMonitor(GpioMonitor&&) = delete;
+    GpioMonitor& operator=(GpioMonitor&&) = delete;
+
+    /** @brief Constructs GpioMonitor object.
+     *
+     *  @param[in] path     - Path to gpio input device
+     *  @param[in] key      - GPIO key to monitor
+     *  @param[in] polarity - GPIO assertion polarity to look for
+     *  @param[in] target   - systemd unit to be started on GPIO
+     *                        value change
+     *  @param[in] event    - sd_event handler
+     *  @param[in] continueRun - Whether to continue after key pressed
+     *  @param[in] handler  - IO callback handler. Defaults to one in this
+     *                        class
+     *  @param[in] useEvDev - Whether to use EvDev to retrieve events
+     */
+    GpioMonitor(gpiod_line* line, gpiod_line_request_config config,
+                boost::asio::io_service& io, const std::string& target,
+                bool continueRun) :
+        gpioLine(line),
+        gpioConfig(config), gpioEventDescriptor(io), target(target),
+        continueAfterKeyPress(continueRun)
+    {
+        requestGPIOEvents();
+    };
+
+  private:
+    /** @brief GPIO line */
+    gpiod_line* gpioLine;
+
+    /** @brief GPIO line configuration */
+    gpiod_line_request_config gpioConfig;
+
+    /** @brief GPIO event descriptor */
+    boost::asio::posix::stream_descriptor gpioEventDescriptor;
+
+    /** @brief Systemd unit to be started when the condition is met */
+    const std::string& target;
+
+    /** @brief If the monitor should continue after key press */
+    bool continueAfterKeyPress;
+
+    /** @brief register handler for gpio event
+     *
+     *  @return  - 0 on success and negative errno otherwise
+     */
+    int requestGPIOEvents();
+
+    /** @brief Handle the GPIO event and starts configured target */
+    void gpioEventHandler();
+};
+
+} // namespace gpio
+} // namespace phosphor
