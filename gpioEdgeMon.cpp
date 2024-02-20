4dde0c849992f184d38587a9b1c48861f8ad4512
Kyle Nieman <kyle.nieman@fii-na.com>
2024-02-20 10:54:02 -0600

    Refactor phosphor-multi-gpio-monitor
    Phosphor-multi-gpio-monitor configures during runtime a GPIO to monitor
    and target(s) to start on an event. This process can be simplified to
    monitoring for an event, and taking action on that event.
    
    The current implementation is refactored so that the monitoring and
    action taken can be configurable and code reused between the different
    phosphor-multi apps. This includes-
    
    1. Change the systemd unit starting functionality into an action class.
    2. Change the gpio monitoring into a monitoring class.
    3. Add gpio configurations that are present in multi-gpio-presence but
       not unique. (Bias and ActiveLow)
    4. Make parsing of targets generic regardless of monitor event type.
    5. Add gpioHandler class to connect the action to the monitor event. In
       this way, a class won't be needed for each action/monitor
       combination.
    
    Testing:
    Ensure no change in behavior.
    
    Change-Id: Icc2f6e2eb6bd149a1661ee1b409987117661c903
    Signed-off-by: Kyle Nieman <kyle.nieman@fii-na.com>

---
+++ gpioEdgeMon.cpp
@@ -0,0 +1,153 @@
+/**
+ * Copyright © 2019 Facebook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gpioEdgeMon.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/bus.hpp>
+
+namespace phosphor
+{
+namespace gpio
+{
+
+void GpioEdgeMonitor::scheduleEventHandler()
+{
+    gpioEventDescriptor.async_wait(
+        boost::asio::posix::stream_descriptor::wait_read,
+        [this](const boost::system::error_code& ec) {
+            if (ec)
+            {
+                /* Monitoring was not cancelled */
+                if (ec != boost::asio::error::operation_aborted)
+                {
+                    lg2::error("{GPIO} event handler error: {ERROR}", "GPIO",
+                               msg, "ERROR", ec.message());
+                }
+                return;
+            }
+
+            handleGpioEvent();
+        });
+}
+
+void GpioEdgeMonitor::handleGpioEvent()
+{
+    gpiod_line_event gpioLineEvent;
+
+    if (gpiod_line_event_read_fd(gpioEventDescriptor.native_handle(),
+                                 &gpioLineEvent) < 0)
+    {
+        lg2::error("Failed to read {GPIO} from fd", "GPIO", msg);
+        return;
+    }
+
+    if (verbose)
+    {
+        if (gpioLineEvent.event_type == GPIOD_LINE_EVENT_RISING_EDGE)
+        {
+            lg2::info("{GPIO} Asserted", "GPIO", msg);
+        }
+        else
+        {
+            lg2::info("{GPIO} Deasserted", "GPIO", msg);
+        }
+    }
+    performEventAction(gpioLineEvent.event_type);
+
+    /* if not required to continue monitoring then return */
+    if (!continueAfterEvent)
+    {
+        return;
+    }
+
+    /* Schedule a wait event */
+    scheduleEventHandler();
+}
+
+int GpioEdgeMonitor::startMonitoring()
+{
+    std::string flags;
+
+    /* The GPIO should be set up for monitoring only once */
+    if (gpioEventDescriptor.is_open())
+    {
+        lg2::error("{GPIO} was already initialized for monitoring.", "GPIO",
+                   msg);
+        return -1;
+    }
+
+    /* Request an event to monitor for respected gpio line */
+    if (gpiod_line_request(gpioLine, &gpioConfig, 0) < 0)
+    {
+        lg2::error("Failed to request {GPIO}: {ERRNO}", "GPIO", msg, "ERRNO",
+                   errno);
+        return -1;
+    }
+
+    int gpioLineFd = gpiod_line_event_get_fd(gpioLine);
+    if (gpioLineFd < 0)
+    {
+        lg2::error("Failed to get fd for {GPIO}", "GPIO", msg);
+        return -1;
+    }
+
+    if (gpioConfig.flags & GPIOD_LINE_REQUEST_FLAG_BIAS_DISABLE)
+    {
+        flags += " Bias DISABLE";
+    }
+    else if (gpioConfig.flags & GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_UP)
+    {
+        flags += " Bias PULL_UP";
+    }
+    else if (gpioConfig.flags & GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_DOWN)
+    {
+        flags += " Bias PULL_DOWN";
+    }
+
+    if (gpioConfig.flags & GPIOD_LINE_REQUEST_FLAG_ACTIVE_LOW)
+    {
+        flags += " ActiveLow";
+    }
+
+    if (!flags.empty())
+    {
+        flags = "[" + flags + "]";
+    }
+
+    lg2::info("{GPIO} {FLAGS} monitoring started", "GPIO", msg, "FLAGS", flags);
+
+    /* Assign line fd to descriptor for monitoring */
+    gpioEventDescriptor.assign(gpioLineFd);
+
+    /* Get line value and convert to expect input */
+    performInitEventAction((gpiod_line_get_value(gpioLine) == 1)
+                               ? GPIOD_LINE_EVENT_RISING_EDGE
+                               : GPIOD_LINE_EVENT_FALLING_EDGE);
+
+    /* Schedule a wait event */
+    scheduleEventHandler();
+
+    return 0;
+}
+
+void GpioEdgeMonitor::stopMonitoring()
+{
+    gpioEventDescriptor.cancel();
+    Monitor::stopMonitoring();
+}
+} // namespace gpio
+} // namespace phosphor
---
+++ gpioHandler.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "gpioEdgeMon.hpp"
+#include "systemdAction.hpp"
+
+namespace phosphor
+{
+namespace gpio
+{
+
+/** @class GpioHandler
+ *  @brief Responsible for catching state change
+ *  condition and taking action if defined.
+ */
+class GpioHandler
+{
+  public:
+    GpioHandler() = delete;
+    ~GpioHandler() = default;
+    GpioHandler(const GpioHandler&) = delete;
+    GpioHandler& operator=(const GpioHandler&) = delete;
+    GpioHandler(GpioHandler&&) = delete;
+    GpioHandler& operator=(GpioHandler&&) = delete;
+
+    /** @brief Constructs GpioHandler object.
+     *
+     *  @param[in] monitorType - Type of signal to monitor
+     *  @param[in] actionType  - Type of action to perform
+     */
+    GpioHandler(std::unique_ptr<Monitor>& monitorType,
+                std::unique_ptr<Action>& actionType) :
+        monObj(std::move(monitorType)),
+        actObj(std::move(actionType))
+    {
+        monObj->performInitEventAction = actObj->initAction;
+        monObj->performEventAction = actObj->eventAction;
+        monObj->startMonitoring();
+    };
+
+  private:
+    /** @brief Monitoring object */
+    std::unique_ptr<Monitor> monObj;
+
+    /** @brief Action object */
+    std::unique_ptr<Action> actObj;
+};
+} // namespace gpio
+} // namespace phosphor
